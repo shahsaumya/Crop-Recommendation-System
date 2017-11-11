9ce8c2a456f7ffd868e0668752672baa1f7a4bd7
sarmishtaVelury <sarmishta.velury@gmail.com>
2017-11-12 04:22:06 +0530

    SVR Test

--- svrtest.py
+++ svrtest.py
@@ -3,24 +3,48 @@ print(__doc__)
 import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
+import csv
+
+from sklearn.model_selection import TimeSeriesSplit
+
+from sklearn import preprocessing
 
 # #############################################################################
 # Generate sample data
-X = np.sort(5 * np.random.rand(40, 1), axis=0)
-y = np.sin(X).ravel()
 
-# #############################################################################
-# Add noise to targets
-y[::5] += 3 * (0.5 - np.random.rand(8))
+X = []
+y = []
+
+reader = csv.reader(open("hums_out.csv"))
+for row in reader:
+	X.append(row[0])
+	y.append(row[1])
+
+y_train = preprocessing.scale(y)
+X = np.asarray(X)
+X = X.astype(np.int)
+for i in X.size():
+	X_train[i] = np.asscalar(X[i])
+
+print(type(X_train[0]))
+
+tscv = TimeSeriesSplit(n_splits=3)
+print(tscv)  
+
+for train_index, test_index in tscv.split(X):
+   print("TRAIN:", train_index, "TEST:", test_index)
+   X_train, X_test = X[train_index], X[test_index]
+   y_train, y_test = y[train_index], y[test_index]
+
 
 # #############################################################################
 # Fit regression model
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 svr_lin = SVR(kernel='linear', C=1e3)
 svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-y_rbf = svr_rbf.fit(X, y).predict(X)
-y_lin = svr_lin.fit(X, y).predict(X)
-y_poly = svr_poly.fit(X, y).predict(X)
+y_rbf = svr_rbf.fit(X_train, y_train).predict(X_test)
+y_lin = svr_lin.fit(X_train, y_train).predict(X_test)
+y_poly = svr_poly.fit(X_train, y_train).predict(X_test)
 
 # #############################################################################
 # Look at the results
@@ -33,3 +57,4 @@ plt.xlabel('data')
 plt.ylabel('target')
 plt.title('Support Vector Regression')
 plt.legend()
+plt.show()
